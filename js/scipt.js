62b7f9f72c1a3b058080bf61ae93eba816d1f155
huydinh <huydinh8246@gmail.com>
2020-09-18 15:15:53 +0700

    merge

--- js/scipt.js
+++ js/scipt.js
@@ -3,7 +3,8 @@ coding challenge 1
 basic operators
 BMI
 */
-
+const fetch = require("node-fetch");
+const request = require('request');
 // var johnHeight, johnMass;
 // var markHeight, markMass;
 
@@ -30,8 +31,18 @@ branch
 baseball score
 */
 
-let john = [89,120,103]
-let mike = [116,94,123]
+// let john = [89,120,103]
+// let mike = [116,94,123]
 
-let total, avr
-total = 0
+// let total, avr
+// total = 0
+let a = 'https://usi-saas.vnexpress.net/index/getreplay?siteid=1002565&objectid=4163085&objecttype=1&id=36917262&limit=12&offset=12&cookie_aid=4jzji2cnwz9ikjae.1598590932&sort_by=like&template_type=1'
+
+// fetch(a)
+//   .then(response => response.json())
+//   .then(data => console.log(data)); 
+
+request(a, function (error, response, body) {
+    let a = JSON.parse(body)
+    console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+});
